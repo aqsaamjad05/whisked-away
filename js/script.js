0b81ccea7fb9ab71e2463bb9738b945e971b7245
Aqsa Amjad <aqsa_amjad@outlook.com>
2025-02-04 00:41:03 +0000

    create infinite scrolling effect on carousel

--- js/script.js
+++ js/script.js
@@ -1,8 +1,23 @@
 const carousel = document.querySelector(".carousel");
 const arrowBtns = document.querySelectorAll(".wrapper i");
 const firstCardWidth = carousel.querySelector(".card").offsetWidth;
+const carouselChildrens = [...carousel.children];
 
 let isDragging = false, startX, startScrollLeft;
+
+// get the number of cards that can fit in the carousel at once
+let cardPerView = Math.round(carousel.offsetWidth / firstCardWidth);
+
+// insert copies of the last few cards to beginning of carousel for infinite scrolling 
+carouselChildrens.slice(-cardPerView).reverse().forEach(card => {
+    carousel.insertAdjacentHTML("afterbegin", card.outerHTML);
+});
+
+// insert copies of the first few cards to end of carousel for infinite scrolling 
+carouselChildrens.slice(0, cardPerView).forEach(card => {
+    carousel.insertAdjacentHTML("beforeend", card.outerHTML);
+});
+
 
 // add event listeners for the arrow buttons to scroll the carousel left and right
 arrowBtns.forEach(btn => {
@@ -30,6 +45,22 @@ const dragStop = () => {
     carousel.classList.remove("dragging");
 }
 
+const infiniteScroll = () => {
+    // if the carousel is at the beginning, scroll to the end
+    if(carousel.scrollLeft === 0) {
+        carousel.classList.add("no-transition");
+        carousel.scrollLeft = carousel.scrollWidth - ( 2 * carousel.offsetWidth);
+        carousel.classList.remove("no-transition");
+    } 
+    // if the carousel is at the end, scroll to the beginning
+    else if(carousel.scrollLeft === carousel.scrollWidth - carousel.offsetWidth){
+        carousel.classList.add("no-transition");
+        carousel.scrollLeft = carousel.offsetWidth;
+        carousel.classList.remove("no-transition");
+    }
+}
+
 carousel.addEventListener("mousedown", dragStart);
 carousel.addEventListener("mousemove", dragging);
-carousel.addEventListener("mouseup", dragStop);
+document.addEventListener("mouseup", dragStop);
+carousel.addEventListener("scroll", infiniteScroll);
